9d1897bec422a5079d95a5419c199d525785d446
Solexgreat <solomonGodkid@gmail.com>
2023-03-05 01:35:02 +0100

    get_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,11 +4,24 @@ import logging
 from typing import List
 
 
+PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
+
 def filter_datum(fields, redaction, message, separator):
     temp = message
     for field in fields:
         temp = re.sub(field + "=.*?" + separator, field + "=" + redaction + separator, temp)
     return temp
+
+def get_logger() -> logging.Logger:
+    """ Returns logger obj  """
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream_handler)
+    return logger
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
@@ -25,7 +38,7 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         """
         method to format the log message.
-        
+
         """
         return filter_datum(self.fields, self.REDACTION, super(RedactingFormatter, self)
                             .format(record), self.SEPARATOR)
