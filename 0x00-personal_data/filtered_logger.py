caec4ea57693986288f1bf368fbac6ff1c235fd9
Solexgreat <solomonGodkid@gmail.com>
2023-03-05 01:38:52 +0100

    set db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,9 @@
 import re
 import logging
 from typing import List
+from mysql.connector import connection
+from os import environ
+
 
 
 PII_FIELDS = ('name', 'email', 'password', 'ssn', 'phone')
@@ -22,6 +25,21 @@ def get_logger() -> logging.Logger:
     stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(stream_handler)
     return logger
+
+def get_db() -> connection.MySQLConnection:
+    """
+    Connect to mysql server with environmental vars
+    """
+    username = environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    db_host = environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = environ.get("PERSONAL_DATA_DB_NAME")
+    connector = connection.MySQLConnection(
+        user=username,
+        password=password,
+        host=db_host,
+        database=db_name)
+    return 
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
