397cbc33de50c29b681da487e8e59752e2594562
Solexgreat <solomonGodkid@gmail.com>
2023-03-08 15:07:38 +0100

    working on it

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -13,7 +13,6 @@ def status() -> str:
     """
     return jsonify({"status": "OK"})
 
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -25,6 +24,10 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/api/v1/unauthorized', strict_slashes=False)
-def unauthorized() -> None:
-    abort(401)
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized():
+     """ GET /api/v1/unauthorized
+    Return:
+      - abort(401)
+    """
+     return abort(401)
