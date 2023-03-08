2463241db284d7c8a5916e868168b0390eb17def
Solexg_great <solomonGodkid@gmail.com>
2023-03-08 16:06:38 +0000

    forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -35,10 +35,10 @@ def unauthorized() -> str:
     return abort(401)
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def Forbidden() -> str:
+def forbidden() -> str:
     """GET /api/v1/unauthorized
-       Return;
-       - abort (403)
+     Return;
+    - abort (403)
     """
     return abort(403)
 
