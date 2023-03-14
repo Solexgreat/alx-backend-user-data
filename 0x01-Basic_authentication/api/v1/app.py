51a55fee1f2d4e58cdc1cb98c17d5a1b9f899666
Solexgreat <solomonGodkid@gmail.com>
2023-03-14 15:46:21 +0100

    Return the user

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.views.auth.auth import Auth
-
+from api.v1.views.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,10 +16,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = os.environ.get("AUTH_TYPE")
 
-#if auth_type == "jwt":
-#    auth = Auth()
-
-auth = Auth()
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+else:
+    auth = Auth()
 
 
 @app.before_request
