8c314f3d847019e0222d04fffdddd5368b8faf3a
Solexgreat <solomonGodkid@gmail.com>
2023-03-17 12:54:36 +0100

    update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -39,3 +39,6 @@ def register_user() -> str:
     except ValueError:
         return jsonify({"message": 
                         "email already registered"}), 400
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
