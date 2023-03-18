f6f02d34235893bfbe183dfbeae8fb78a652496f
Solexgreat <solomonGodkid@gmail.com>
2023-03-18 04:05:24 +0100

    auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,6 +3,8 @@
 """
 from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 app = Flask(__name__)
@@ -39,6 +41,25 @@ def register_user() -> str:
     except ValueError:
         return jsonify({"message": 
                         "email already registered"}), 400
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """Validate the Login info 
+       and create a login session
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    user = AUTH.valid_login(email, password)
+    if user:
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email,
+                        "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else: 
+        abort(401)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
