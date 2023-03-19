59316275359794edd528f723506b49442ee3bd19
Solexgreat <solomonGodkid@gmail.com>
2023-03-19 05:25:51 +0100

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -121,7 +121,7 @@ def update_password():
     except Exception:
         abort(403)
 
-    return jsonify({"email": email, "message": "Password updated"})
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
